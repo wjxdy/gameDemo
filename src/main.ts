993ebad60181a96c472ef7018b3aa4de36b28a17
特仑苏 <2077152374@qq.com>
2026-05-30 22:53:37 +0800

    chore: scaffold phaser project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+import './style.css';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 960,
+  height: 640,
+  backgroundColor: '#0f172a',
+  pixelArt: true,
+  roundPixels: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
+  scene: [],
+};
+
+new Phaser.Game(config);
+
